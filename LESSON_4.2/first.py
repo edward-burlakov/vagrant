814f47c753f52a6341859703fc80cc95eea1d947
edward-burlakov <edward.burlakov@gmail.com>
2022-07-31 11:59:20 +0000

    Modifications staged

--- LESSON_4.2/first.py
+++ LESSON_4.2/first.py
@@ -8,23 +8,26 @@ curruser = str(os.getlogin())
 print("Текущий пользователь:", curruser )
 
 # Выводим на экран рабочий каталог пользователя
-currdir = os.getcwd()
-print("Рабочий каталог:", currdir)
+workdir = os.getcwd()
+print("Рабочий каталог:", workdir)
 
 # Определяем домашний каталог пользователя
 homedir = pwd.getpwuid(os.getuid()).pw_dir
 
 # Переходим в каталог  с исходниками git
 os.chdir( homedir+"/netology/sysadm-homeworks")
-currdir = os.getcwd()
-print("Смена текущего рабочего каталога на проверяемый: {0} ".format( currdir ))
+gitdir = os.getcwd()
+print("Смена текущего рабочего каталога на проверяемый: {0} ".format( gitdir ))
 
 # Получаем статус git репозитория
 bash_command = ["git status"]
 
+print('\033[1;33;40m')
+print("Изменённые файлы:")
 # Читаем результат выполнения команды.
 result_os = os.popen(' && '.join(bash_command)).read()
 for result in result_os.split('\n'):
     if result.find('modified') != -1:
          prepare_result = result.replace('\tmodified:   ', '')
-         print(prepare_result)
+         print( os.getcwd()+'/'+ prepare_result )
+print('\033[0m')
