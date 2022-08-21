47bd72f7ddab27aef9e6e94885dad08fe03af5f3
Edward Burlakov <edwardb@mail.ru>
2022-08-22 00:31:18 +0700

    Lesson 4.3 Commit 35
    Signed-off-by: Edward Burlakov <edwardb@mail.ru>

--- LESSON_4.3/script_4.3.py
+++ LESSON_4.3/script_4.3.py
@@ -14,20 +14,16 @@ def inc(n):
     return n
 
 ### Задаем интервал проверок в секундах.
-
 wait_sec = 4
 
 ### Устанавливаем счетчик итераций проверок.
-
 n = 1
 
 ### Создаем объект словаря с серверами и их исходными IP адресами:
-
 webservers = {'drive.google.com': '2.2.2.2', 'mail.google.com': '1.1.1.1', 'google.com': '8.8.8.8'}
 print("Наши сервера:", webservers)
 
 # Функция заполнения объекта словаря актуальными IP адресами
-
 def fill_dictionary(x):
     for node in x:
         ip = socket.gethostbyname(node)
@@ -35,26 +31,22 @@ def fill_dictionary(x):
     return x
 
 # Функция формирования словаря в формате json в файле  webservers.json
-
 def fill_json(y):
     with open('webservers.json', 'w') as jtmp:
         jtmp.write(str(json.dumps(y)))        # Сериализация объекта Python в строку формата JASON
     return
 
 # Функция формирования словаря в формате yaml в файле  webservers.yaml
-
 def fill_yaml(z):
     with open('webservers.yaml', 'w') as ytmp:
         ytmp.write(yaml.dump(z))              # Сериализация объекта Python в строку формата YAML
     return
 
 # Заполняем YAML и JSON файлы, чтобы в них был актуальный список адресов
-
 fill_json(fill_dictionary(webservers))
 fill_yaml(fill_dictionary(webservers))
 
 while True:
-
         tmp = fill_dictionary(webservers)   # Создаем временную копию словаря.
         for host in webservers:
             ip = s.gethostbyname(host)  # Обращаемся в интернет и получаем очередной IP по имени хоста.
